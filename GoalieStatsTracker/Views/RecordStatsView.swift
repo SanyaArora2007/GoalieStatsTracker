e6b4ec1dae35c1e50e283a5f309b07e0d933e65f
Sanya Arora <sanya.r.arora@gmail.com>
2024-01-14 17:15:59 -0600

    discard button

--- GoalieStatsTracker/Views/RecordStatsView.swift
+++ GoalieStatsTracker/Views/RecordStatsView.swift
@@ -228,22 +228,22 @@ struct RecordStatsView: View {
                 
                 Spacer()
                     .frame(height: 60)
-                
-                HStack {
-                    Button(
-                        action: {
-                            showSaveAlert = true
-                            Task {
-                                do {
-                                    try await save()
+
+                if loadPastView == false {
+                    HStack {
+                        Button(
+                            action: {
+                                showSaveAlert = true
+                                Task {
+                                    do {
+                                        try await save()
+                                    }
+                                    catch {
+                                        fatalError(error.localizedDescription)
+                                    }
                                 }
-                                catch {
-                                    fatalError(error.localizedDescription)
-                                }
-                            }
-                        },
-                        label: {
-                            if loadPastView == false {
+                            },
+                            label: {
                                 ZStack {
                                     Text("Save")
                                         .foregroundStyle(.teal)
@@ -263,35 +263,37 @@ struct RecordStatsView: View {
                                         }
                                 }
                             }
-                        }
-                    )
-                    
-                    Spacer()
-                        .frame(width: 90)
-                    
-                    Button(
-                        action: {
-                            showDiscardAlert = true
-                        },
-                        label: {
-                            Text("Discard")
-                                .foregroundStyle(.teal)
-                                .font(.title)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .stroke(Color.gray, lineWidth: 4)
-                                        .frame(width: 150, height: 55)
-                                )
-                                .alert(isPresented: $showDiscardAlert) {
-                                    Alert(title: Text("Disacrd Game"), message: Text("Are you sure you want to discard this game?"), dismissButton: Alert.Button.default(
-                                        Text("Yes"), action: {
-                                            presentationMode.wrappedValue.dismiss()
-                                        }
+                        )
+                        
+                        Spacer()
+                            .frame(width: 90)
+                        
+                        Button(
+                            action: {
+                                showDiscardAlert = true
+                            },
+                            label: {
+                                Text("Discard")
+                                    .foregroundStyle(.teal)
+                                    .font(.title)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(Color.gray, lineWidth: 4)
+                                            .frame(width: 150, height: 55)
                                     )
-                                    )
-                                }
-                        }
-                    )
+                                    .alert(isPresented: $showDiscardAlert) {
+                                        Alert(
+                                            title: Text("Disacrd Game"),
+                                            message: Text("Are you sure you want to discard this game?"),
+                                            primaryButton: Alert.Button.default(
+                                                Text("Yes"), action: { presentationMode.wrappedValue.dismiss() }
+                                            ),
+                                            secondaryButton: .cancel()
+                                        )
+                                    }
+                            }
+                        )
+                    }
                 }
             }
             .disabled(disable)
