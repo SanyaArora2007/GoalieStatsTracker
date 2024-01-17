81d64567da778aebd00e769b58d9371245f24d76
Rishi Arora <rishi.r.arora@gmail.com>
2024-01-16 16:56:51 -0800

    Responsive sizing in scoring view

--- GoalieStatsTracker/Views/ScoringView.swift
+++ GoalieStatsTracker/Views/ScoringView.swift
@@ -12,36 +12,40 @@ struct ScoringView: View {
     
     var _parent: RecordStatsView
     let _geometry: GeometryProxy
+    let shotsFontSize: CGFloat = 0.025
+    let scoreFontSize: CGFloat = 0.04
 
     init(parent: RecordStatsView, geometry: GeometryProxy) {
         _parent = parent
         _geometry = geometry
     }
     
+    func scoreColor() -> Color {
+        if _parent.shotsData.runningScore < 0 {
+            return Color.red
+        }
+        else if _parent.shotsData.runningScore > 0 {
+            return Color.blue
+        }
+        else {
+            return Color.black
+        }
+    }
+    
     var body: some View {
         VStack {
-            if _parent.shotsData.runningScore < 0 {
-                Text(String(format: "Running Score: %.1f", _parent.shotsData.runningScore))
-                    .foregroundColor(Color.red)
-                .font(Font.title)                }
-            else if _parent.shotsData.runningScore > 0 {
-                Text(String(format: "Running Score: %.1f", _parent.shotsData.runningScore))
-                    .foregroundColor(Color.blue)
-                .font(Font.title)                }
-            else if _parent.shotsData.runningScore == 0 {
-                Text(String(format: "Running Score: %.1f", _parent.shotsData.runningScore))
-                    .foregroundColor(Color.black)
-                    .font(Font.title)
-            }
-            
+            Text(String(format: "Running Score: %.1f", _parent.shotsData.runningScore))
+                .foregroundColor(scoreColor())
+                .font(.system(size: _geometry.size.height * scoreFontSize))
+
             Text("Saves: \(_parent.shotsData.saves)   (\(_parent.shotsData.savePercentage)%)")
                 .foregroundColor(Color.black)
-                .font(Font.headline)
+                .font(.system(size: _geometry.size.height * shotsFontSize))
                 .frame(alignment: .trailing)
             
             Text("Total Shots: \(_parent.shotsData.totalShots)")
                 .foregroundColor(Color.black)
-                .font(Font.headline)
+                .font(.system(size: _geometry.size.height * shotsFontSize))
                 .frame(alignment: .trailing)
         }
 
